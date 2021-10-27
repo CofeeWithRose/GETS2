125a84b435ce9e7e7306b4b0a779faa72d31db1f
grape <grapepeng@tencent.com>
2021-10-27 17:47:41 +0800

    remove inputsystem

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,31 +1,22 @@
 import EntityManagerSystem from "../systems/entity/implement/EntityManagerSystem";
-import {InputSystem} from "../systems/input/implement/InputSystem";
 import {TimerSystem} from "../systems/timer/implement/TimerSystem";
 import TaskSystem from "../systems/task/implemet/TaskSystem";
 import {taskConig} from "./TaskConig";
 import { InitConfigInterface, SystemConfig } from "../core/interface/InitConfigInterface";
 import { Fps } from "../systems/Fps";
 import { Transformer } from "../systems/Transformer";
-import { KeyBoard } from "../systems/input/interface/data/enum";
 
 export const createConfig = (
   canvas: HTMLCanvasElement, 
-  {defaultKeys}: {defaultKeys: KeyBoard[]}
 ): InitConfigInterface =>  ({
-
     systemConfig:[
         {
             systemConstructor: TaskSystem,
             config: taskConig,
         },
-     
         {
             systemConstructor: TimerSystem,
             config: {},
-        },
-        {
-            systemConstructor: InputSystem,
-            config: {defaultKeys},
         },
         {
             systemConstructor: EntityManagerSystem,
