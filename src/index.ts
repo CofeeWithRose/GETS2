59c3e1783a9c96c1efc0d1af1201cc20852cb31a
cofeewithrose <1836071395@qq.com>
2021-11-21 20:32:47 +0800

    publish

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export { AbstractComponent } from "./core/implement/AbstractComponent";
 // export { Render2DComp, Render2DCompInfer } from './components/render2D/implement/Render2DComp'
 // export * from './components/animation2D/Animation'
 export { Transform, Vec2, TransformInfer } from './components/Transform'
-export { TimerSystem as TimerManager} from "./systems/timer/implement/TimerSystem";
 export type { FunComponent } from './core/interface/AbstractComponentInterface'
 export { AbstractSystem } from './core/implement/AbstractSystem'
 export { AbstractComponentLoader, EntityManagerSystem }
