7d988a01a6c66a5bcb9a6619d2f71e360f0197ac
grape <grapepeng@tencent.com>
2021-11-30 14:25:24 +0800

    add getComponentTypeChain util

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ export { createConfig } from './configs'
 export {InitConfigInterface} from "./core/interface/InitConfigInterface";
 export {Entity} from "./systems/entity/implement/data/Entity";
 export { AbstractComponent } from "./core/implement/AbstractComponent";
-// export { Render2DComp, Render2DCompInfer } from './components/render2D/implement/Render2DComp'
-// export * from './components/animation2D/Animation'
 export { Transform, Vec2, TransformInfer } from './components/Transform'
 export type { FunComponent } from './core/interface/AbstractComponentInterface'
 export { AbstractSystem } from './core/implement/AbstractSystem'
