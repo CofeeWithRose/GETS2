0c06a19374e7f56d3b7f5aadd5f7ab9077dda81d
grape <grapepeng@tencent.com>
2021-12-10 07:05:21 +0800

    test canvas 2d text

--- src/stories/example/canvas-test.sb.tsx
+++ src/stories/example/canvas-test.sb.tsx
@@ -5,20 +5,41 @@ export default {
     component: Canvas2dText,
 }
 
+type FixedTextMetrics = TextMetrics & {fontBoundingBoxAscent: number, fontBoundingBoxDescent: number}
+
+const getTextTexture = ( text: string, font: string, lineHeight: number) => {
+    const texture = document.createElement('canvas')
+    const ctx = texture.getContext('2d')
+    if(!ctx) return
+    ctx.font = font; 
+    const textMetrics = ctx.measureText(text) as FixedTextMetrics
+    const { fontBoundingBoxAscent, fontBoundingBoxDescent, width } = textMetrics
+    texture.height = lineHeight
+    texture.width = width
+
+    const fontHeight = fontBoundingBoxAscent + fontBoundingBoxDescent
+    const lineHeighOffset = (lineHeight - fontHeight) * 0.5
+    ctx.font = font; 
+    ctx.fillStyle = '#ffffff'
+    ctx.fillText(text, 0, fontBoundingBoxAscent + lineHeighOffset)
+    return texture
+}
 
 export function Canvas2dText() {
 
     const canvasRef = useRef<(HTMLCanvasElement|null)[]>([])
     const text = '杀害'
-    const fontSize = 12;
-    const fontFamily = 'auto'
-    const lineHeight = '2'
-    // fantasy
-    // auto
-    const font = `bold ${Math.max(fontSize, 12)}px/${lineHeight} ${fontFamily}`
+    const fontSize = 28;
+    const fontFamily = 'Arial'
+    const lineHeightStr = '2'
+    const lineHeight = lineHeightStr.includes('px')? parseInt(lineHeightStr) : fontSize *  parseInt(lineHeightStr)
+    const font = `bold ${Math.max(fontSize, 12)}px/${lineHeight}px ${fontFamily}`
     const positioon = {x: 0, y: 50}
     const canvasSize= {width: 500, height: 500}
     const canvasStyleSize = { width: canvasSize.width/devicePixelRatio , height: canvasSize.height/devicePixelRatio }
+
+
+   
     useEffect(() => {
         const canvasList = canvasRef.current
         canvasList.forEach(canvas => {
@@ -26,33 +47,36 @@ export function Canvas2dText() {
             const ctx = canvas.getContext('2d')
             if(!ctx) return
             ctx.scale(devicePixelRatio, devicePixelRatio)
-            ctx.font = font;
-            ctx.textAlign = 'left';
-            ctx.fillStyle = 'blue'
-            const textMetrics = ctx.measureText(text)
-            const {
-                fontBoundingBoxAscent, fontBoundingBoxDescent, 
-                actualBoundingBoxAscent, actualBoundingBoxDescent,
-                width
-            } = textMetrics
+            const texture = getTextTexture(text, font, lineHeight)
+            console.log(texture.height, texture.width);
+            
+            ctx.drawImage(texture, positioon.x, positioon.y)
+            // ctx.font = font;
+            // ctx.textAlign = 'left';
+            // ctx.textBaseline = 'middle'
+            // ctx.fillStyle = 'blue'
+            // const textMetrics = ctx.measureText(text)
+          
+            // const {
+            //     fontBoundingBoxAscent, fontBoundingBoxDescent, 
+            //     actualBoundingBoxAscent, actualBoundingBoxDescent,
+            //     width
+            // } = textMetrics
 
-            const boundaryHeight = fontBoundingBoxDescent + fontBoundingBoxAscent
-            const realLineHeight = lineHeight.includes('px')? parseInt(lineHeight) : fontSize *  parseInt(lineHeight)
-            const offsetY = (realLineHeight - boundaryHeight) * 0.5
+            
+            // const boundaryHeight = fontBoundingBoxDescent + fontBoundingBoxAscent
+            // const offsetY = (lineHeight - boundaryHeight) * 0.5
+            // console.log('fontBoundingBoxAscent, actualBoundingBoxAscent', fontBoundingBoxAscent, actualBoundingBoxAscent);
 
-            ctx.fillStyle = 'blue'
-            ctx.fillText( text, positioon.x, (positioon.y + fontBoundingBoxAscent + offsetY))
+            // const first = positioon.y + fontBoundingBoxAscent + offsetY
+            // ctx.fillStyle = 'blue'
+            // ctx.fillText( text, positioon.x, first)
 
-            ctx.fillText( text, positioon.x, (
-                positioon.y + fontBoundingBoxAscent + offsetY + 
-                boundaryHeight + 2*offsetY
-            ))
+            // ctx.fillText( text, positioon.x, (
+            //     first + lineHeight
+            // ))
 
-            ctx.fillText( text, positioon.x, (
-                positioon.y + fontBoundingBoxAscent + offsetY + 
-                boundaryHeight + 2*offsetY +
-                boundaryHeight +  2*offsetY
-            ))
+            // ctx.fillText( text, positioon.x, (first + lineHeight *2))
         })
        
 
